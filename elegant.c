42bf24f604239126d37448b3410146472e9575a1
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2000-01-07 15:40:27 +0000

    Updated version message.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@5306 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- elegant.c
+++ elegant.c
@@ -29,9 +29,9 @@ void traceback_handler(int code);
 char *option[N_OPTIONS] = {
     "describeinput",
         };
-char *USAGE="elegant <inputfile>\n\nProgram by Michael Borland. (This is version 13.16, December 1999.)";
+char *USAGE="elegant <inputfile>\n\nProgram by Michael Borland. (This is version 13.17, January 2000.)";
 
-char *GREETING="This is elegant, by Michael Borland. (This is version 13.16, December 1999.)";
+char *GREETING="This is elegant, by Michael Borland. (This is version 13.17, January 2000.)";
 
 #define RUN_SETUP        0
 #define RUN_CONTROL      1
