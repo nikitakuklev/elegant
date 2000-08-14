c0755842d39563a809893d4f7591989073c9e283
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2000-08-14 21:44:57 +0000

    First version.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@6065 c1b61890-85f3-ea29-e4d6-a272fe79f168

---
+++ sddsanalyzebeam.c
@@ -0,0 +1,368 @@
+/*CopyrightNotice001
+
+*****************************************************************
+                          COPYRIGHT NOTIFICATION
+*****************************************************************
+
+THE FOLLOWING IS A NOTICE OF COPYRIGHT, AVAILABILITY OF THE CODE,
+AND DISCLAIMER WHICH MUST BE INCLUDED IN THE PROLOGUE OF THE CODE
+AND IN ALL SOURCE LISTINGS OF THE CODE.
+ 
+(C)  COPYRIGHT 1998 UNIVERSITY OF CHICAGO
+ 
+Argonne National Laboratory (ANL), with facilities in the States of 
+Illinois and Idaho, is owned by the United States Government, and
+operated by the University of Chicago under provision of a contract
+with the Department of Energy.
+
+Portions of this material resulted from work developed under a U.S.
+Government contract and are subject to the following license:  For
+a period of five years from December 30, 1998, the Government is
+granted for itself and others acting on its behalf a paid-up,
+nonexclusive, irrevocable worldwide license in this computer
+software to reproduce, prepare derivative works, and perform
+publicly and display publicly.  With the approval of DOE, this
+period may be renewed for two additional five year periods. 
+Following the expiration of this period or periods, the Government
+is granted for itself and others acting on its behalf, a paid-up,
+nonexclusive, irrevocable worldwide license in this computer
+software to reproduce, prepare derivative works, distribute copies
+to the public, perform publicly and display publicly, and to permit
+others to do so.
+
+*****************************************************************
+                                DISCLAIMER
+*****************************************************************
+
+NEITHER THE UNITED STATES GOVERNMENT NOR ANY AGENCY THEREOF, NOR
+THE UNIVERSITY OF CHICAGO, NOR ANY OF THEIR EMPLOYEES OR OFFICERS,
+MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LEGAL
+LIABILITY OR RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR
+USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY
+OWNED RIGHTS.  
+
+*****************************************************************
+LICENSING INQUIRIES MAY BE DIRECTED TO THE INDUSTRIAL TECHNOLOGY
+DEVELOPMENT CENTER AT ARGONNE NATIONAL LABORATORY.
+
+CopyrightNotice001*/
+
+/* program: sddsanalyzebeam.c
+ * purpose: take particle coordinate input and analyze to give
+ *          moments and twiss parameters
+ *
+ * Michael Borland, 2000
+ *
+ $Log: not supported by cvs2svn $
+ */
+#include "mdb.h"
+#include "scan.h"
+#include "SDDS.h"
+
+#define DEBUG 0
+
+#define SET_PIPE 0
+#define SET_NOWARNINGS 1
+#define N_OPTIONS 2
+
+char *option[N_OPTIONS] = {
+  "pipe", "nowarnings"
+} ;
+
+char *USAGE="sddsanalyzebeam [-pipe=[input][,output]] [<SDDSinputfile>] [<SDDSoutputfile>]\n\
+  [-nowarnings]\n\
+Computes Twiss parameters and other properties of a particle beam.\n\
+The input file must have columns x, xp, y, yp, and p; for example, an elegant\n\
+beam output file is acceptable.\n\
+Program by Michael Borland.  (This is version 1, August 2000.)\n";
+
+
+long SetUpOutputFile(SDDS_DATASET *SDDSout, char *outputfile);
+long check_sdds_beam_column(SDDS_TABLE *SDDS_table, char *name, char *units);
+
+char *CenName[6];
+char *CorName[6][6];
+char *psName[6] = {"x", "xp", "y", "yp", "t", "delta" };
+  
+main(int argc, char **argv)
+{
+  SDDS_DATASET SDDSin, SDDSout;
+  char *inputfile, *outputfile;
+  long iPart, particles, i_arg, readCode, noWarnings, row;
+  long i, j;
+  SCANNED_ARG *s_arg;
+  unsigned long pipeFlags;
+  double *x, *xp, *y, *yp, *p, *t;
+  double pAve, sum;
+  double S[6][6], C[6], beta[2], alpha[2], eta[4], emit[2], emitcor[2], beamsize[6];
+  double *data[6], Sbeta[4][4];
+  long tmpFileUsed;
+  
+  SDDS_RegisterProgramName(argv[0]);
+  argc = scanargs(&s_arg, argc, argv);
+  if (argc<2) 
+    bomb(NULL, USAGE);
+
+  inputfile = outputfile = NULL;
+  pipeFlags = noWarnings = 0;
+  
+  for (i_arg=1; i_arg<argc; i_arg++) {
+    if (s_arg[i_arg].arg_type==OPTION) {
+      switch (match_string(s_arg[i_arg].list[0], option, N_OPTIONS, 0)) {
+      case SET_PIPE:
+        if (!processPipeOption(s_arg[i_arg].list+1, s_arg[i_arg].n_items-1, &pipeFlags))
+          SDDS_Bomb("invalid -pipe syntax");
+        break;
+      case SET_NOWARNINGS:
+        noWarnings = 1;
+        break;
+      default:
+        fprintf(stdout, "error: unknown switch: %s\n", s_arg[i_arg].list[0]);
+        fflush(stdout);
+        exit(1);
+        break;
+      }
+    }
+    else {
+      if (inputfile==NULL)
+        inputfile = s_arg[i_arg].list[0];
+      else if (outputfile==NULL)
+        outputfile = s_arg[i_arg].list[0];
+      else
+        SDDS_Bomb("too many filenames");
+    }
+  }
+
+  processFilenames("sddsanalyzebeam", &inputfile, &outputfile, pipeFlags, noWarnings, &tmpFileUsed);
+  if (tmpFileUsed)
+    SDDS_Bomb("can't overwrite input file");
+  
+  if (!SDDS_InitializeInput(&SDDSin, inputfile))
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+
+  /* check the input file for valid data */
+  if (!check_sdds_beam_column(&SDDSin, "x", "m") || !check_sdds_beam_column(&SDDSin, "y", "m") ||
+      !check_sdds_beam_column(&SDDSin, "xp", NULL) || !check_sdds_beam_column(&SDDSin, "yp", NULL) ||
+      !check_sdds_beam_column(&SDDSin, "t", "s") ||
+      (!check_sdds_beam_column(&SDDSin, "p", "m$be$nc") && !check_sdds_beam_column(&SDDSin, "p", NULL))) {
+    fprintf(stderr, 
+            "sddsanalyzebeam: one or more of (x, xp, y, yp, p) have the wrong units or are not present in %s", 
+            inputfile);
+    exit(1);
+  }
+
+  if (!SetUpOutputFile(&SDDSout, outputfile))
+    SDDS_Bomb("problem setting up output file");
+  
+  row = 0;
+  while ((readCode=SDDS_ReadPage(&SDDSin))>0) {
+    if (readCode!=1 && !SDDS_LengthenTable(&SDDSout, 1))
+      SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+    for (i=0; i<2; i++)
+      beta[i] = alpha[i] = emit[i] = emitcor[i] = eta[i] = eta[i+2] = 0;
+    for (i=0; i<6; i++)
+      for (j=C[i]=0; j<=i; j++)
+        S[i][j] = 0;
+    if ((particles=SDDS_RowCount(&SDDSin))>2) {
+      if (!(data[0] = x = SDDS_GetColumnInDoubles(&SDDSin, "x")) ||
+          !(data[1] = xp = SDDS_GetColumnInDoubles(&SDDSin, "xp")) ||
+          !(data[2] = y = SDDS_GetColumnInDoubles(&SDDSin, "y")) ||
+          !(data[3] = yp = SDDS_GetColumnInDoubles(&SDDSin, "yp")) ||
+          !(data[4] = t =  SDDS_GetColumnInDoubles(&SDDSin, "t")) ||
+          !(data[5] = p = SDDS_GetColumnInDoubles(&SDDSin, "p")))
+        SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+      /* compute and subtract off average values */
+      for (i=0; i<5; i++) {
+        C[i] = arithmeticAverage(data[i], particles);
+        for (j=0; j<particles; j++)
+          data[i][j] -= C[i];
+      }
+      /* convert momentum to (p-po)/po */
+      C[6] = 0;
+      pAve = arithmeticAverage(p, particles);
+      for (i=0; i<particles; i++)
+        p[i] = p[i]/pAve-1;
+
+      /* compute correlations */
+      for (i=0; i<6; i++) {
+        for (j=0; j<=i; j++) {
+          for (iPart=sum=0; iPart<particles; iPart++)
+            sum += data[i][iPart]*data[j][iPart];
+          S[j][i] = S[i][j] = sum/particles;
+        }
+      }
+      for (i=0; i<6; i++)
+        beamsize[i] = sqrt(S[i][i]);
+
+      /* compute correlations with energy correlations removed */
+      if (S[5][5])
+        for (i=0; i<4; i++) 
+          eta[i] = S[i][5]/S[5][5];
+      else 
+        for (i=0; i<4; i++) 
+          eta[i] = 0;
+      for (i=0; i<4; i++) {
+        for (iPart=0; iPart<particles; iPart++)
+          data[i][iPart] -= eta[i]*data[5][iPart];
+      }
+      for (i=0; i<4; i++) {
+        for (j=0; j<4; j++) {
+          for (iPart=sum=0; iPart<particles; iPart++)
+            sum += data[i][iPart]*data[j][iPart];
+          Sbeta[j][i] = Sbeta[i][j] = sum/particles;
+        }
+      }
+      /* compute beta functions etc */
+      for (i=0; i<2; i++) {
+        emitcor[i] = emit[i] = beta[i] = alpha[i] = 0;
+        if ((emit[i] = S[2*i+0][2*i+0]*S[2*i+1][2*i+1]-sqr(S[2*i+0][2*i+1]))>0)
+          emit[i] = sqrt(emit[i]);
+        if ((emitcor[i] = Sbeta[2*i+0][2*i+0]*Sbeta[2*i+1][2*i+1]-sqr(Sbeta[2*i+0][2*i+1]))>0) {
+          emitcor[i] = sqrt(emitcor[i]);
+          beta[i] = Sbeta[2*i+0][2*i+0]/emitcor[i];
+          alpha[i] = -Sbeta[2*i+0][2*i+1]/emitcor[i];
+        }
+      }
+    }
+    /* set centroids and sigmas */
+    for (i=0; i<6; i++) {
+      if (!SDDS_SetRowValues(&SDDSout, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE,
+                             row, CenName[i], C[i], NULL)) {
+        SDDS_SetError("Problem setting centroid value");
+        SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+      }
+      for (j=0; j<=i; j++) {
+        if (!SDDS_SetRowValues(&SDDSout, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE,
+                               row, CorName[i][j], S[i][j], NULL)) {
+          SDDS_SetError("Problem setting sigma value");
+          SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+        }
+      }
+    }
+    if (!SDDS_SetRowValues(&SDDSout, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE, row,
+                           "Sx", beamsize[0], "Sxp", beamsize[1],
+                           "Sy", beamsize[2], "Syp", beamsize[3],
+                           "St", beamsize[4], "Sdelta", beamsize[5], NULL)) {
+      SDDS_SetError("Problem setting sigma value");
+      SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+    }
+    
+    /* set twiss parameter, etc */
+    if (!SDDS_SetRowValues(&SDDSout, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE, row, 
+                           "betax", beta[0], "alphax", alpha[0], "etax", eta[0],
+                           "etaxp", eta[1], "ex", emit[0], "ecx", emitcor[0],
+                           "enx", emit[0]*pAve, "ecnx", emitcor[0]*pAve,
+                           "betay", beta[1], "alphay", alpha[1], "etay", eta[2],
+                           "etayp", eta[3], "ey", emit[1], 
+                           "ecy", emitcor[1], "eny", emit[1]*pAve, "ecny", emitcor[1]*pAve,
+                           "pAverage", pAve, NULL)) {
+      SDDS_SetError("Problem setting Twiss values");
+      SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+    }
+    row++;
+  }
+  if (readCode==0)
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+  if (!SDDS_WritePage(&SDDSout) || !SDDS_Terminate(&SDDSin) ||
+      !SDDS_Terminate(&SDDSout))
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+  return 0;
+}
+
+
+long SetUpOutputFile(SDDS_DATASET *SDDSout, char *outputfile)
+{
+  char units[128], name[128];
+  char *ppUnits[6] = {"m", "", "m", "", "s", ""} ;
+  long i, j;
+  
+  if (!SDDS_InitializeOutput(SDDSout, SDDS_BINARY, 1, NULL, NULL, outputfile) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "ex", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "enx", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "ecx", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "ecnx", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "betax", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "alphax", NULL, SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "etax", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "etaxp", NULL, SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "ey", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "eny", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "ecy", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "ecny", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "betay", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "alphay", NULL, SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "etay", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "etayp", NULL, SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "Sx", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "Sxp", NULL, SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "Sy", "m", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "Syp", NULL, SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "St", "s", SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "Sdelta", NULL, SDDS_DOUBLE) ||
+      !SDDS_DefineSimpleColumn(SDDSout, "pAverage", NULL, SDDS_DOUBLE)) {
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+  }
+  for (i=0; i<6; i++) {
+    sprintf(name, "C%s", psName[i]);
+    if (!SDDS_CopyString(&CenName[i], name))
+      SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+    if (!SDDS_DefineSimpleColumn(SDDSout, name, ppUnits[i], SDDS_DOUBLE))
+      SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+    for (j=0; j<=i; j++) {
+      sprintf(name, "s%ld%ld", j+1, i+1);
+      units[0] = 0;
+      if (ppUnits[i]) {
+        if (ppUnits[j]) {
+          if (strcmp(ppUnits[i], ppUnits[j])==0 && strlen(ppUnits[i]))
+            sprintf(units, "%s$a2$n", ppUnits[i]);
+          else if (strlen(ppUnits[i]))
+            sprintf(units, "%s %s", ppUnits[i], ppUnits[j]);
+        }
+        else {
+          strcpy(units, ppUnits[i]);
+        }
+      } else if (ppUnits[j])
+        strcpy(units, ppUnits[j]);
+      if (!SDDS_DefineSimpleColumn(SDDSout, name, units, SDDS_DOUBLE))
+        SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+      if (!SDDS_CopyString(&CorName[i][j], name))
+        SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+    }
+  }
+  if (!SDDS_SaveLayout(SDDSout) || !SDDS_WriteLayout(SDDSout) ||
+      !SDDS_StartPage(SDDSout, 1)) {
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+  }
+}
+
+
+
+long check_sdds_beam_column(SDDS_TABLE *SDDS_table, char *name, char *units)
+{
+  char *units1;
+  if (SDDS_GetColumnIndex(SDDS_table, name)<0)
+    return(0);
+  if (SDDS_GetColumnInformation(SDDS_table, "units", &units1, SDDS_GET_BY_NAME, name)!=SDDS_STRING) {
+    SDDS_SetError("units field of column has wrong data type!");
+    SDDS_PrintErrors(stderr, SDDS_EXIT_PrintErrors|SDDS_VERBOSE_PrintErrors);
+  }
+  if (!units) {
+    if (!units1)
+      return(1);
+    if (SDDS_StringIsBlank(units1)) {
+      free(units1);
+      return(1);
+    }
+    return(0);
+  }
+  if (!units1)
+    return(0);
+  if (strcmp(units, units1)==0) {
+    free(units1);
+    return(1);
+  }
+  free(units1);
+  return(0);
+}
+
---
+++ sddsmatchtwiss.c
@@ -0,0 +1,308 @@
+/*CopyrightNotice001
+
+*****************************************************************
+                          COPYRIGHT NOTIFICATION
+*****************************************************************
+
+THE FOLLOWING IS A NOTICE OF COPYRIGHT, AVAILABILITY OF THE CODE,
+AND DISCLAIMER WHICH MUST BE INCLUDED IN THE PROLOGUE OF THE CODE
+AND IN ALL SOURCE LISTINGS OF THE CODE.
+ 
+(C)  COPYRIGHT 1998 UNIVERSITY OF CHICAGO
+ 
+Argonne National Laboratory (ANL), with facilities in the States of 
+Illinois and Idaho, is owned by the United States Government, and
+operated by the University of Chicago under provision of a contract
+with the Department of Energy.
+
+Portions of this material resulted from work developed under a U.S.
+Government contract and are subject to the following license:  For
+a period of five years from December 30, 1998, the Government is
+granted for itself and others acting on its behalf a paid-up,
+nonexclusive, irrevocable worldwide license in this computer
+software to reproduce, prepare derivative works, and perform
+publicly and display publicly.  With the approval of DOE, this
+period may be renewed for two additional five year periods. 
+Following the expiration of this period or periods, the Government
+is granted for itself and others acting on its behalf, a paid-up,
+nonexclusive, irrevocable worldwide license in this computer
+software to reproduce, prepare derivative works, distribute copies
+to the public, perform publicly and display publicly, and to permit
+others to do so.
+
+*****************************************************************
+                                DISCLAIMER
+*****************************************************************
+
+NEITHER THE UNITED STATES GOVERNMENT NOR ANY AGENCY THEREOF, NOR
+THE UNIVERSITY OF CHICAGO, NOR ANY OF THEIR EMPLOYEES OR OFFICERS,
+MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LEGAL
+LIABILITY OR RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR
+USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY
+OWNED RIGHTS.  
+
+*****************************************************************
+LICENSING INQUIRIES MAY BE DIRECTED TO THE INDUSTRIAL TECHNOLOGY
+DEVELOPMENT CENTER AT ARGONNE NATIONAL LABORATORY.
+
+CopyrightNotice001*/
+
+/* program: sddsmatchtwiss.c
+ * purpose: take particle coordinate input and convert the Twiss parameters
+ *          to user-supplied values.
+ *
+ * Michael Borland, 2000
+ *
+ $Log: not supported by cvs2svn $
+ */
+#include "mdb.h"
+#include "scan.h"
+#include "SDDS.h"
+
+#define DEBUG 0
+
+#define SET_PIPE 0
+#define SET_XPLANE 1
+#define SET_YPLANE 2
+#define SET_NOWARNINGS 3
+#define N_OPTIONS 4
+
+char *option[N_OPTIONS] = {
+  "pipe", "xplane", "yplane", "nowarnings"
+} ;
+
+char *USAGE="sddsmatchtwiss [-pipe=[input][,output]] [<SDDSinputfile>] [<SDDSoutputfile>]\n\
+  -xPlane=[beta=<meters>,alpha=<value>][,etaValue=<meters>][,etaSlope=<value>]\n\
+  -yPlane=[beta=<meters>][,alpha=<value>][,etaValue=<meters>][,etaSlope=<value>]\n\
+  [-nowarnings]\n\
+The input file must have columns x, xp, y, yp, and p; for example, an elegant\n\
+beam output file is acceptable.\n\
+beta and alpha must be given together, or omitted together.\n\
+etaValue and etaSlope may be given individually or together.  If etaValue is not given,\n\
+the coordinates have no dispersion adjustment.  If etaSlope is not given, then\n\
+slopes have no dispersion adjustment.\n\n\
+Program by Michael Borland.  (This is version 1, August 2000.)\n";
+
+typedef struct {
+  double beta, alpha, eta, etap;
+  unsigned long flags;
+#define BETA_GIVEN  0x0001UL
+#define ALPHA_GIVEN 0x0002UL
+#define ETA_GIVEN   0x0004UL
+#define ETAP_GIVEN  0x0008UL
+} PLANE_SPEC;
+
+long PerformTransformation(double *x, double *xp, double *p, long rows, PLANE_SPEC match);
+long check_sdds_beam_column(SDDS_TABLE *SDDS_table, char *name, char *units);
+
+
+main(int argc, char **argv)
+{
+  SDDS_DATASET SDDSin, SDDSout;
+  char *inputfile, *outputfile;
+  long i_arg, row, rows, readCode, noWarnings;
+  SCANNED_ARG *s_arg;
+  unsigned long pipeFlags;
+  PLANE_SPEC xSpec, ySpec;
+  double *x, *xp, *y, *yp, *p;
+  
+  SDDS_RegisterProgramName(argv[0]);
+  argc = scanargs(&s_arg, argc, argv);
+  if (argc<2) 
+    bomb(NULL, USAGE);
+
+  inputfile = outputfile = NULL;
+  pipeFlags = noWarnings = 0;
+  xSpec.flags = ySpec.flags = 0;
+  
+  for (i_arg=1; i_arg<argc; i_arg++) {
+    if (s_arg[i_arg].arg_type==OPTION) {
+      switch (match_string(s_arg[i_arg].list[0], option, N_OPTIONS, 0)) {
+      case SET_XPLANE:
+        xSpec.flags = 0;
+        s_arg[i_arg].n_items--;
+        if (!scanItemList(&xSpec.flags, s_arg[i_arg].list+1, &s_arg[i_arg].n_items, 0,
+                          "beta", SDDS_DOUBLE, &xSpec.beta, 1, BETA_GIVEN,
+                          "alpha", SDDS_DOUBLE, &xSpec.alpha, 1, ALPHA_GIVEN,
+                          "etavalue", SDDS_DOUBLE, &xSpec.eta, 1, ETA_GIVEN,
+                          "etaslope", SDDS_DOUBLE, &xSpec.etap, 1, ETAP_GIVEN,
+                          NULL) ||
+            (xSpec.flags&BETA_GIVEN && !(xSpec.flags&ALPHA_GIVEN)) ||
+            (!(xSpec.flags&BETA_GIVEN) && xSpec.flags&ALPHA_GIVEN) ||
+            (xSpec.flags&BETA_GIVEN && xSpec.beta<=0))
+          SDDS_Bomb("invalid -xPlane syntax/values---watch out for abbreviations of etaValue and etaSlope");
+        break;
+      case SET_YPLANE:
+        ySpec.flags = 0;
+        s_arg[i_arg].n_items--;
+        if (!scanItemList(&ySpec.flags, s_arg[i_arg].list+1, &s_arg[i_arg].n_items, 0,
+                          "beta", SDDS_DOUBLE, &ySpec.beta, 1, BETA_GIVEN,
+                          "alpha", SDDS_DOUBLE, &ySpec.alpha, 1, ALPHA_GIVEN,
+                          "etavalue", SDDS_DOUBLE, &ySpec.eta, 1, ETA_GIVEN,
+                          "etaslope", SDDS_DOUBLE, &ySpec.etap, 1, ETAP_GIVEN,
+                          NULL) ||
+            (ySpec.flags&BETA_GIVEN && !(ySpec.flags&ALPHA_GIVEN)) ||
+            (!(ySpec.flags&BETA_GIVEN) && ySpec.flags&ALPHA_GIVEN) ||
+            (ySpec.flags&BETA_GIVEN && ySpec.beta<=0))
+          SDDS_Bomb("invalid -yPlane syntax/values---watch out for abbreviations of etaValue and etaSlope");
+        break;
+      case SET_PIPE:
+        if (!processPipeOption(s_arg[i_arg].list+1, s_arg[i_arg].n_items-1, &pipeFlags))
+          SDDS_Bomb("invalid -pipe syntax");
+        break;
+      case SET_NOWARNINGS:
+        noWarnings = 1;
+        break;
+      default:
+        fprintf(stdout, "error: unknown switch: %s\n", s_arg[i_arg].list[0]);
+        fflush(stdout);
+        exit(1);
+        break;
+      }
+    }
+    else {
+      if (inputfile==NULL)
+        inputfile = s_arg[i_arg].list[0];
+      else if (outputfile==NULL)
+        outputfile = s_arg[i_arg].list[0];
+      else
+        SDDS_Bomb("too many filenames");
+    }
+  }
+
+  processFilenames("sddsmatchbeam", &inputfile, &outputfile, pipeFlags, noWarnings, NULL);
+
+  if (!SDDS_InitializeInput(&SDDSin, inputfile))
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+
+  /* check the input file for valid data */
+  if (!check_sdds_beam_column(&SDDSin, "x", "m") || !check_sdds_beam_column(&SDDSin, "y", "m") ||
+      !check_sdds_beam_column(&SDDSin, "xp", NULL) || !check_sdds_beam_column(&SDDSin, "yp", NULL) ||
+      (!check_sdds_beam_column(&SDDSin, "p", "m$be$nc") && !check_sdds_beam_column(&SDDSin, "p", NULL))) {
+    fprintf(stderr, 
+            "sddsmatchtwiss: one or more data quantities (x, xp, y, yp, p) have the wrong units or are not present in %s", 
+            inputfile);
+    exit(1);
+  }
+
+  if (!SDDS_InitializeCopy(&SDDSout, &SDDSin, outputfile, "w") ||
+      !SDDS_SaveLayout(&SDDSout) || !SDDS_WriteLayout(&SDDSout))
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+  
+  while ((readCode=SDDS_ReadPage(&SDDSin))>0) {
+    if ((rows=SDDS_RowCount(&SDDSin))==0) {
+      if (!SDDS_CopyPage(&SDDSin, &SDDSout) || !SDDS_WritePage(&SDDSout))
+        SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+      continue;
+    }
+    if (!(x = SDDS_GetColumnInDoubles(&SDDSin, "x")) ||
+        !(xp = SDDS_GetColumnInDoubles(&SDDSin, "xp")) ||
+        !(y = SDDS_GetColumnInDoubles(&SDDSin, "y")) ||
+        !(yp = SDDS_GetColumnInDoubles(&SDDSin, "yp")) ||
+        !(p = SDDS_GetColumnInDoubles(&SDDSin, "p")))
+      SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+    PerformTransformation(x, xp, p, rows, xSpec);
+    PerformTransformation(y, yp, p, rows, ySpec);
+    if (!SDDS_CopyPage(&SDDSout, &SDDSin) ||
+        !(SDDS_SetColumnFromDoubles(&SDDSout, SDDS_SET_BY_NAME, x, rows, "x")) ||
+        !(SDDS_SetColumnFromDoubles(&SDDSout, SDDS_SET_BY_NAME, y, rows, "y")) ||
+        !(SDDS_SetColumnFromDoubles(&SDDSout, SDDS_SET_BY_NAME, xp, rows, "xp")) ||
+        !(SDDS_SetColumnFromDoubles(&SDDSout, SDDS_SET_BY_NAME, yp, rows, "yp")) ||
+        !(SDDS_SetColumnFromDoubles(&SDDSout, SDDS_SET_BY_NAME, p, rows, "p")) ||
+        !SDDS_WritePage(&SDDSout))
+      SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+  }
+  if (readCode==0)
+    SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
+}
+
+long PerformTransformation(double *x, double *xp, double *p, long rows, PLANE_SPEC match)
+{
+  long i;
+  double pAve;
+  double S11, S22, S12, S16, S26, S66;
+  double R11, R12, R21, R22;
+  double x0, xp0;
+  double emit, beta1, beta2, alpha1, alpha2;
+  double eta1, etap1, eta2, etap2;
+  
+  pAve = arithmeticAverage(p, rows);
+  for (i=0; i<rows; i++)
+    p[i] = p[i]/pAve - 1;
+  computeCorrelations(&S11, &S16, &S66, x, p, rows);
+  eta1 = S16/S66;
+  for (i=0; i<rows; i++)
+    x[i] -= p[i]*eta1;
+  computeCorrelations(&S22, &S26, &S66, xp, p, rows);
+  etap1 = S26/S66;
+  for (i=0; i<rows; i++)
+    xp[i] -= p[i]*etap1;
+  
+  if (match.flags&BETA_GIVEN) {
+    computeCorrelations(&S11, &S12, &S22, x, xp,rows);
+    if ((emit = S11*S22-sqr(S12))<0) {
+      SDDS_Bomb("emittance is zero");
+    }
+    else
+      emit = sqrt(emit);
+    beta1 = S11/emit;
+    alpha1 = -S12/emit;
+    beta2 = match.beta;
+    alpha2 = match.alpha;
+    R11 = beta2/sqrt(beta1*beta2);
+    R12 = 0;
+    R21 = (alpha1-alpha2)/sqrt(beta1*beta2);
+    R22 = beta1/sqrt(beta1*beta2);
+    for (i=0; i<rows; i++) {
+      x0 = x[i];
+      xp0 = xp[i];
+      x[i] = R11*x0 + R12*xp0;
+      xp[i] = R21*x0 + R22*xp0;
+    }
+  }
+
+  if (match.flags&ETA_GIVEN)
+    eta2 = match.eta;
+  else
+    eta2 = eta1;
+  if (match.flags&ETAP_GIVEN)
+    etap2 = match.etap;
+  else
+    etap2 = etap1;
+  for (i=0; i<rows; i++) {
+    x[i] += eta2*p[i];
+    xp[i] += etap2*p[i];
+    p[i] = (p[i]+1)*pAve;
+  }
+  return 1;
+}
+
+long check_sdds_beam_column(SDDS_TABLE *SDDS_table, char *name, char *units)
+{
+  char *units1;
+  if (SDDS_GetColumnIndex(SDDS_table, name)<0)
+    return(0);
+  if (SDDS_GetColumnInformation(SDDS_table, "units", &units1, SDDS_GET_BY_NAME, name)!=SDDS_STRING) {
+    SDDS_SetError("units field of column has wrong data type!");
+    SDDS_PrintErrors(stderr, SDDS_EXIT_PrintErrors|SDDS_VERBOSE_PrintErrors);
+  }
+  if (!units) {
+    if (!units1)
+      return(1);
+    if (SDDS_StringIsBlank(units1)) {
+      free(units1);
+      return(1);
+    }
+    return(0);
+  }
+  if (!units1)
+    return(0);
+  if (strcmp(units, units1)==0) {
+    free(units1);
+    return(1);
+  }
+  free(units1);
+  return(0);
+}
+
