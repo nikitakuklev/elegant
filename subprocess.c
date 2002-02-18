cf51d2e5d552443ca7087097bb208d1484e405f2
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2002-02-18 17:16:33 +0000

    Recognize %% syntax to insert a literal %.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@7581 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- subprocess.c
+++ subprocess.c
@@ -25,58 +25,67 @@ static int pid;
 
 void run_subprocess(NAMELIST_TEXT *nltext, RUN *run)
 {
-    static char buffer[1024];
-    char *ptr, *ptr0;
-    void dummy_sigusr1();
+  static char buffer[1024];
+  char *ptr, *ptr0;
+  void dummy_sigusr1();
 
-    log_entry("run_subprocess");
+  log_entry("run_subprocess");
 
 #if !defined(_WIN32)
-    signal(SIGUSR1, dummy_sigusr1);
+  signal(SIGUSR1, dummy_sigusr1);
 #endif
-    if (!fp) {
-        /* open a pipe and start csh */
-        fp = popen("csh", "w");
-        pid = getpid();
-        }
+  if (!fp) {
+    /* open a pipe and start csh */
+    fp = popen("csh", "w");
+    pid = getpid();
+  }
 
-    /* process the namelist text */
-    set_namelist_processing_flags(STICKY_NAMELIST_DEFAULTS);
-    set_print_namelist_flags(0);
-    process_namelist(&subprocess, nltext);
-    print_namelist(stdout, &subprocess);
+  /* process the namelist text */
+  set_namelist_processing_flags(STICKY_NAMELIST_DEFAULTS);
+  set_print_namelist_flags(0);
+  process_namelist(&subprocess, nltext);
+  print_namelist(stdout, &subprocess);
 
-    if (command) {
-        buffer[0] = 0;
-        ptr0 = command;
-        while ((ptr=strstr(ptr0, "%s"))) {
-            if (!run || !run->rootname)
-                bomb("rootname must be initialized prior to subprocess execution if \%s substitution is used", NULL);
-            *ptr = 0;
-            ptr += 2;
-            strcat(buffer, ptr0);
-            strcat(buffer, run->rootname);
-            ptr0 = ptr;
-            }
+  if (command) {
+    buffer[0] = 0;
+    ptr0 = command;
+    while ((ptr=strstr(ptr0, "%s"))) {
+      if (ptr!=command && *(ptr-1)=='%') {
+        *(ptr-1) = 0;
         strcat(buffer, ptr0);
-        fprintf(stdout, "%s\n", buffer);
-        fflush(stdout);
-        fprintf(fp, "%s\nkill -USR1 %d\n", buffer, pid);
-        fflush(fp);
+        strcat(buffer, "%s");
+        ptr += 2;
+        ptr0 = ptr;
+      }
+      else {
+        if (!run || !run->rootname)
+          bomb("rootname must be initialized prior to subprocess execution if \%s substitution is used", NULL);
+        *ptr = 0;
+        ptr += 2;
+        strcat(buffer, ptr0);
+        strcat(buffer, run->rootname);
+        ptr0 = ptr;
+      }
+    }
+    strcat(buffer, ptr0);
+    fprintf(stdout, "%s\n", buffer);
+    fflush(stdout);
+    fprintf(fp, "%s\nkill -USR1 %d\n", buffer, pid);
+    fflush(fp);
 #if !defined(_WIN32)
-        /* pause until SIGUSR1 is received */
-        sigpause(SIGUSR1);
+    /* pause until SIGUSR1 is received */
+    sigpause(SIGUSR1);
 #endif
-        }
+  }
 
 #if !defined(_WIN32)               
-    /* back to default behavior for sigusr1 */
-    signal(SIGUSR1, SIG_DFL);
+  /* back to default behavior for sigusr1 */
+  signal(SIGUSR1, SIG_DFL);
 #endif
-    log_exit("run_subprocess");
-    }
+  log_exit("run_subprocess");
+}
 
 /* dummy signal handler for use with sigpause */
 void subprocess_sigusr1()
 {
-    }
+}
