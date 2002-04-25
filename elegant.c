32c2e2443aacbefd242540ea2cc764bbc783b777
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2002-04-25 14:44:47 +0000

    Changed version message to 14.7Beta1
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@7834 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- elegant.c
+++ elegant.c
@@ -31,9 +31,9 @@ char *option[N_OPTIONS] = {
     "describeinput",
     "macro",
         };
-char *USAGE="elegant <inputfile> [-macro=<tag>=<value>,[...]]\n\nProgram by Michael Borland. (This is version 14.6.1, "__DATE__".)";
+char *USAGE="elegant <inputfile> [-macro=<tag>=<value>,[...]]\n\nProgram by Michael Borland. (This is version 14.7Beta1, "__DATE__".)";
 
-char *GREETING="This is elegant, by Michael Borland. (This is version 14.6.1, "__DATE__".)";
+char *GREETING="This is elegant, by Michael Borland. (This is version 14.7Beta1, "__DATE__".)";
 
 #define RUN_SETUP        0
 #define RUN_CONTROL      1
