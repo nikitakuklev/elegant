6cf48567e83e56a5406cee7acd8e757cbc441f11
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2003-04-17 01:58:11 +0000

    Improved functionality (should actually be accepted by COSY).
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@8998 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- convert_to_cosy.c
+++ convert_to_cosy.c
@@ -118,8 +118,6 @@ void emitCosyDrift(FILE *fp, char *name, double length)
   fprintf(fp, "  ENDPROCEDURE ;\n");
 }
 
-
-
 void emitCosyDipole(FILE *fp, char *name,
                     double length, double angle, 
                     double e1, double e2,
@@ -143,17 +141,22 @@ void emitCosyDipole(FILE *fp, char *name,
   for (i=1; i<=4; i++) 
     for (j=2; j<=4; j++)
       fprintf(fp, "    W(%ld, %ld) := 0 ;\n", i, j);
-  computeEngeCoefficients(engeCoef, rho, length, 2*hgap, fint);
+
   if (angle<0) 
     fprintf(fp, "    CB ;\n");
-  fprintf(fp, "    FR 3 ;\n");
-  fprintf(fp, "    FC 1 1 1 %.15g %.15g %.15g 0.0 0.0 0.0 ; \n",
-          engeCoef[0], engeCoef[1], engeCoef[2]);
-  fprintf(fp, "    FC 1 2 1 %.15g %.15g %.15g 0.0 0.0 0.0 ; \n",
-          engeCoef[0], engeCoef[1], engeCoef[2]);
-  fprintf(fp, "    MSS %.15g %.15g %.15g %.15g %.15g %.15g %.15g W; \n",
+  if (hgap!=0 && fint!=0) {
+    computeEngeCoefficients(engeCoef, rho, length, 2*hgap, fint);
+    fprintf(fp, "    FR 3 ;\n");
+    fprintf(fp, "    FC 1 1 1 %.15g %.15g %.15g\n       0.0 0.0 0.0 ; \n",
+            engeCoef[0], engeCoef[1], engeCoef[2]);
+    fprintf(fp, "    FC 1 2 1 %.15g %.15g %.15g\n       0.0 0.0 0.0 ; \n",
+            engeCoef[0], engeCoef[1], engeCoef[2]);
+  } else {
+    fprintf(fp, "    FR 1 ;\n");
+  }
+  fprintf(fp, "    MSS %.15g %.15g %.15g %.15g\n       %.15g %.15g %.15g W; \n",
           rho, 180/PI*fabs(angle), 
-          hgap, 
+          hgap==0?1e-3:hgap, 
           sign*180/PI*e1, h1, 
           sign*180/PI*e2, h2);
   if (angle<0) 
@@ -181,37 +184,55 @@ void convert_to_cosy(char *outputfile, LINE_LIST *beamline,
     FILE *fp;
     double BRho;
     long i;
-    
+    char *ptr;
+#define NVAR 6
+    char *varName[NVAR] = {
+      "A", "B", "G", "R", "MU", "F"
+      };
+    char **nameUsed = NULL;
+    long namesUsed = 0;
+
     fp = fopen_e(outputfile, "w", 0);
 
     fprintf(fp, "INCLUDE 'COSY';\n");
     fprintf(fp, "PROCEDURE RUN ;\n");
-
-    /* user-defined order, 6D phase space, no knobs */
-    fprintf(fp, "OV %ld 3 0 ;\n", cosyOrder);
-    /* electrons with given momentum */
-    fprintf(fp, "RPE %.15g ;\n", (sqrt(pCentral*pCentral+1)-1)*me_mks*sqr(c_mks)/e_mks/1e6);
-    /* initialize map to unity */
-    fprintf(fp, "UM ;\n");
-    
+    for (i=0; i<NVAR; i++)
+      fprintf(fp, "    VARIABLE %s 100 2 ; \n", varName[i]);
+      
     BRho = pCentral*me_mks*c_mks/e_mks;
 
     /* emit procedures describing elements */
     eptr = &(beamline->elem);
     while (eptr) {
+      while (ptr=strchr(eptr->name, ':'))
+        *ptr = '_';
+      for (i=0; i<namesUsed; i++) {
+        if (strcmp(eptr->name, nameUsed[i])==0)
+          break;
+      }
+      if (i!=namesUsed) {
+        eptr = eptr->succ;
+        continue;
+      }
+      nameUsed = trealloc(nameUsed, sizeof(*nameUsed)*(namesUsed+1));
+      nameUsed[namesUsed++] = eptr->name;
       switch (eptr->type) {
       case T_QUAD:
         quad = (QUAD*)eptr->p_elem;
         fprintf(fp, "  PROCEDURE %s ;\n", eptr->name);
+        fprintf(fp, "    { K1 = %e, R = %e, BRho = %e }\n",
+                quad->k1, quadBoreRadius, BRho);
         fprintf(fp, "    MQ %.15g %.15g %.15g ; \n",
-                quad->length, quad->k1*BRho*quadBoreRadius, quadBoreRadius);
+                quad->length, -quad->k1*BRho*quadBoreRadius, quadBoreRadius);
         fprintf(fp, "  ENDPROCEDURE; \n");
         break;
       case T_KQUAD:
         kquad = (KQUAD*)eptr->p_elem;
         fprintf(fp, "  PROCEDURE %s ;\n", eptr->name);
+        fprintf(fp, "    { K1 = %e, R = %e, BRho = %e }\n",
+                kquad->k1, quadBoreRadius, BRho);
         fprintf(fp, "    MQ %.15g %.15g %.15g ; \n",
-                kquad->length, kquad->k1*BRho*quadBoreRadius, quadBoreRadius);
+                kquad->length, -kquad->k1*BRho*quadBoreRadius, quadBoreRadius);
         fprintf(fp, "  ENDPROCEDURE; \n");
         break;
       case T_SBEN:
@@ -246,6 +267,8 @@ void convert_to_cosy(char *outputfile, LINE_LIST *beamline,
       case T_SEXT:
         sext = (SEXT*)eptr->p_elem;
         fprintf(fp, "  PROCEDURE %s ;\n", eptr->name);
+        fprintf(fp, "    { K2 = %e, R = %e, BRho = %e }\n",
+                sext->k2, sextBoreRadius, BRho);
         fprintf(fp, "    MSK %.15g %.15g %.15g ;\n",
                 sext->length, sext->k2*BRho*sextBoreRadius, sextBoreRadius);
         fprintf(fp, "  ENDPROCEDURE; \n");
@@ -253,6 +276,8 @@ void convert_to_cosy(char *outputfile, LINE_LIST *beamline,
       case T_KSEXT:
         ksext = (KSEXT*)eptr->p_elem;
         fprintf(fp, "  PROCEDURE %s ;\n", eptr->name);
+        fprintf(fp, "    { K2 = %e, R = %e, BRho = %e }\n",
+                ksext->k2, sextBoreRadius, BRho);
         fprintf(fp, "    MSK %.15g %.15g %.15g ;\n",
                 ksext->length, ksext->k2*BRho*sextBoreRadius, sextBoreRadius);
         fprintf(fp, "  ENDPROCEDURE; \n");
@@ -301,6 +326,15 @@ void convert_to_cosy(char *outputfile, LINE_LIST *beamline,
     }
     fprintf(fp, "\n  ENDPROCEDURE ;\n");
     
+    fprintf(fp, "    OV %ld 2 1 ;\n", cosyOrder);
+    fprintf(fp, "    RPE %.15g*PARA(1) ;\n", (sqrt(pCentral*pCentral+1)-1)*me_mks*sqr(c_mks)/e_mks/1e6);
+    fprintf(fp, "    UM ; MACH; \n");
+    fprintf(fp, "    TP MU; WRITE 7 ' DELTA-DEPENDENT TUNES: '   MU(1) MU(2) ;\n");
+    fprintf(fp, "    GT MAP F MU A B G R;\n");
+    fprintf(fp, "    WRITE 7 ' DELTA-DEPENDENT FIXED POINT ' F(1) F(2) F(3) F(4) ;\n");
+    fprintf(fp, "    WRITE 7 ' DELTA-DEPENDENT ALPHAS ' A(1) A(2) ;\n");
+    fprintf(fp, "    WRITE 7 ' DELTA-DEPENDENT BETAS  ' B(1) B(2) ;\n");
+    fprintf(fp, "    WRITE 7 ' DELTA-DEPENDENT GAMMAS ' G(1) G(2) ;\n");
     fprintf(fp, "ENDPROCEDURE ;\n");
     fprintf(fp, "RUN ;\n");
     fprintf(fp, "END ;\n");
