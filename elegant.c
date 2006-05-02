0f857ce3df0f95453131f514c4f643fb618dc5b8
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2006-05-02 21:08:43 +0000

    Updated version to 16.0.1
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@12691 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- elegant.c
+++ elegant.c
@@ -60,13 +60,13 @@ char *option[N_OPTIONS] = {
     "cpulist",
         };
 #if USE_MPI
-char *USAGE="mpirun -np <number of processes> Pelegant <inputfile> [-macro=<tag>=<value>,[...]]\n\nProgram by Yusong Wang, Michael Borland. (This is version 16.0Beta, "__DATE__".)";
+char *USAGE="mpirun -np <number of processes> Pelegant <inputfile> [-macro=<tag>=<value>,[...]]\n\nProgram by Yusong Wang, Michael Borland. (This is version 16.0.1, "__DATE__".)";
 
-char *GREETING="This is parallel elegant, by Yusong Wang, Michael Borland. (This is version 16.0Beta, "__DATE__".)";
+char *GREETING="This is parallel elegant, by Yusong Wang, Michael Borland. (This is version 16.0.1, "__DATE__".)";
 #else
-char *USAGE="elegant <inputfile> [-macro=<tag>=<value>,[...]] [-cpuList=<number>[,<number>]]\n\nProgram by Michael Borland. (This is version 16.0Beta, "__DATE__".)";
+char *USAGE="elegant <inputfile> [-macro=<tag>=<value>,[...]] [-cpuList=<number>[,<number>]]\n\nProgram by Michael Borland. (This is version 16.0.1, "__DATE__".)";
 
-char *GREETING="This is elegant, by Michael Borland. (This is version 16.0Beta, "__DATE__".)";
+char *GREETING="This is elegant, by Michael Borland. (This is version 16.0.1, "__DATE__".)";
 #endif
 
 #define RUN_SETUP        0
