3397e898e018a2a654e2c1d9f188eb221c0fa110
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2008-06-19 21:40:31 +0000

    Added ElementType column.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@14660 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- output_magnets.c
+++ output_magnets.c
@@ -34,19 +34,23 @@ void output_magnets(char *filename, char *line_name, LINE_LIST *beamline)
     start = end = 0;
     fprintf(fpm, "SDDS1\n&description text=\"magnet layout for beamline %s\" &end\n", line_name);
     fprintf(fpm, "&column name=ElementName, type=string &end\n");
+    fprintf(fpm, "&column name=ElementType, type=string &end\n");
     fprintf(fpm, "&column name=s, units=m, type=double &end\n&column name=Profile, type=double &end\n");
     fprintf(fpm, "&data mode=ascii, no_row_counts=1 &end\n");
 
     eptr = &(beamline->elem);
-    fprintf(fpm, "_BEGIN_ 0 0\n");
+    fprintf(fpm, "_BEGIN_ MARK 0 0\n");
     while (eptr) {
          switch (eptr->type) {
             case T_QUAD:
                 qptr = (QUAD*)eptr->p_elem;
-                fprintf(fpm, "%s %e  %d\n", eptr->name, start, SIGN(qptr->k1));
+                fprintf(fpm, "%s %s %e  %d\n", eptr->name, entity_name[eptr->type], start, SIGN(qptr->k1));
                 end = start+qptr->length;
-                fprintf(fpm, "%s %e  %d\n%s %e  0\n%s %e 0 %\n%s %e 0\n", eptr->name, 
-                        end, SIGN(qptr->k1), eptr->name, end, eptr->name, start, eptr->name, end);
+                fprintf(fpm, "%s %s %e  %d\n%s %s %e  0\n%s %s %e 0 %\n%s %s %e 0\n",
+                        eptr->name, entity_name[eptr->type], end, SIGN(qptr->k1), 
+                        eptr->name, entity_name[eptr->type], end, 
+                        eptr->name, entity_name[eptr->type], start,
+                        eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_KQUAD:
@@ -55,10 +59,13 @@ void output_magnets(char *filename, char *line_name, LINE_LIST *beamline)
                     value = kqptr->B;
                 else
                     value = kqptr->k1;
-                fprintf(fpm, "%s %e  %d\n", eptr->name, start, SIGN(kqptr->k1));
+                fprintf(fpm, "%s %s %e  %d\n", eptr->name, entity_name[eptr->type], start, SIGN(kqptr->k1));
                 end = start+kqptr->length;
-                fprintf(fpm, "%s %e  %d\n%s %e  0\n%s %e 0 %\n%s %e 0\n", eptr->name, 
-                        end, SIGN(kqptr->k1), eptr->name, end, eptr->name, start, eptr->name, end);
+                fprintf(fpm, "%s %s %e  %d\n%s %s %e  0\n%s %s %e 0 %\n%s %s %e 0\n", 
+                        eptr->name, entity_name[eptr->type], end, SIGN(kqptr->k1), 
+                        eptr->name, entity_name[eptr->type], end, 
+                        eptr->name, entity_name[eptr->type], start,
+                        eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_RBEN:  case T_SBEN:
@@ -66,14 +73,14 @@ void output_magnets(char *filename, char *line_name, LINE_LIST *beamline)
                 end  = start+bptr->length;
                 if (bptr->angle>0)
                     fprintf(fpm, 
-                            "%s %e .33333333\n%s %e .33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, eptr->name, start, 
-                            eptr->name, start, eptr->name, end);
+                            "%s %s %e .33333333\n%s %s %e .33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], start, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 else if (bptr->angle<0)
                     fprintf(fpm, 
-                            "%s %e -.33333333\n%s %e -.33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, eptr->name, start, 
-                            eptr->name, start, eptr->name, end);
+                            "%s %s %e -.33333333\n%s %s %e -.33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], start, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_KSBEND:
@@ -81,78 +88,78 @@ void output_magnets(char *filename, char *line_name, LINE_LIST *beamline)
                 end  = start+kbptr->length;
                 if (kbptr->angle>0)
                     fprintf(fpm, 
-                            "%s %e .33333333\n%s %e .33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, eptr->name, start, 
-                            eptr->name, start, eptr->name, end);
+                            "%s %s %e .33333333\n%s %s %e .33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], start, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 else if (kbptr->angle<0)
                     fprintf(fpm, 
-                            "%s %e -.33333333\n%s %e -.33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, eptr->name, start, 
-                            eptr->name, start, eptr->name, end);
+                            "%s %s %e -.33333333\n%s %s %e -.33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], start, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_SEXT:
                 end = start+((SEXT*)eptr->p_elem)->length;
-                fprintf(fpm, "%s %e  .5\n%s %e .5\n%s %e 0\n%s %e 0\n%s %e 0\n", 
-                        eptr->name, start, eptr->name, end, eptr->name, end, eptr->name, start, 
-                        eptr->name, end);
+                fprintf(fpm, "%s %s %e  .5\n%s %s %e .5\n%s %s %e 0\n%s %s %e 0\n%s %s %e 0\n", 
+                        eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], start, 
+                        eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_KSEXT:
                 end = start+((KSEXT*)eptr->p_elem)->length;
-                fprintf(fpm, "%s %e  .5\n%s %e .5\n%s %e 0\n%s %e 0\n%s %e 0\n", 
-                        eptr->name, start, eptr->name, end, eptr->name, end, eptr->name, start, 
-                        eptr->name, end);
+                fprintf(fpm, "%s %s %e  .5\n%s %s %e .5\n%s %s %e 0\n%s %s %e 0\n%s %s %e 0\n", 
+                        eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], start, 
+                        eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_HCOR:
                 end    = start+((HCOR*)eptr->p_elem)->length;
-                fprintf(fpm, "%s %e .25\n%s %e .25\n%s %e 0\n", 
-                        eptr->name, start, eptr->name, end, eptr->name, end);
+                fprintf(fpm, "%s %s %e .25\n%s %s %e .25\n%s %s %e 0\n", 
+                        eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_VCOR:
                 end    = start+((VCOR*)eptr->p_elem)->length;
-                fprintf(fpm, "%s %e -.25\n%s %e -.25\n%s %e 0\n", 
-                        eptr->name, start, eptr->name, end, eptr->name, end);
+                fprintf(fpm, "%s %s %e -.25\n%s %s %e -.25\n%s %s %e 0\n", 
+                        eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_HVCOR:
                 end    = start+((HVCOR*)eptr->p_elem)->length;
-                fprintf(fpm, "%s %e .25\n%s %e -.25\n%s %e 0\n",
-                        eptr->name, start, eptr->name, end, eptr->name, end);
+                fprintf(fpm, "%s %s %e .25\n%s %s %e -.25\n%s %s %e 0\n",
+                        eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_DRIF:
                 start = (end = start+((DRIFT*)eptr->p_elem)->length);
-                fprintf(fpm, "%s %e  0\n", eptr->name, end);
+                fprintf(fpm, "%s %s %e  0\n", eptr->name, entity_name[eptr->type], end);
                 break;
             case T_HMON:
                 dz = ((HMON*)eptr->p_elem)->length/2;
-                fprintf(fpm, "%s %e 0.125\n%s %e 0\n%s %e 0\n%s %e 0\n",
-                        eptr->name, start+dz, eptr->name, start+2*dz, 
-                        eptr->name, start+dz, eptr->name, start+2*dz);
+                fprintf(fpm, "%s %s %e 0.125\n%s %s %e 0\n%s %s %e 0\n%s %s %e 0\n",
+                        eptr->name, entity_name[eptr->type], start+dz, eptr->name, entity_name[eptr->type], start+2*dz, 
+                        eptr->name, entity_name[eptr->type], start+dz, eptr->name, entity_name[eptr->type], start+2*dz);
                 start += 2*dz;
                 break;
             case T_VMON:
                 dz = ((VMON*)eptr->p_elem)->length/2;
-                fprintf(fpm, "%s %e -0.125\n%s %e 0\n%s %e 0\n%s %e 0\n",
-                        eptr->name, start+dz, eptr->name, start+2*dz, 
-                        eptr->name, start+dz, eptr->name, start+2*dz);
+                fprintf(fpm, "%s %s %e -0.125\n%s %s %e 0\n%s %s %e 0\n%s %s %e 0\n",
+                        eptr->name, entity_name[eptr->type], start+dz, eptr->name, entity_name[eptr->type], start+2*dz, 
+                        eptr->name, entity_name[eptr->type], start+dz, eptr->name, entity_name[eptr->type], start+2*dz);
                 start += 2*dz;
                 break;
             case T_MONI:
                 dz = ((MONI*)eptr->p_elem)->length/2;
-                fprintf(fpm, "%s %e 0.125\n%s %e 0\n%s %e -0.125\n%s %e 0\n%s %e 0\n",
-                        eptr->name, start+dz, eptr->name, start+2*dz, eptr->name, start+dz, 
-                        eptr->name, start, eptr->name, start+2*dz);
+                fprintf(fpm, "%s %s %e 0.125\n%s %s %e 0\n%s %s %e -0.125\n%s %s %e 0\n%s %s %e 0\n",
+                        eptr->name, entity_name[eptr->type], start+dz, eptr->name, entity_name[eptr->type], start+2*dz, eptr->name, entity_name[eptr->type], start+dz, 
+                        eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], start+2*dz);
                 start += 2*dz;
                 break;
             case T_MULT:
                 dz = ((MULT*)eptr->p_elem)->length/3;
-                fprintf(fpm, "%s %e 0.6666\n%s %e 0.6666\n%s %e 0\n%s %e -0.6666\n%s %e -0.6666\n%s %e 0\n%s %e 0\n",
-                        eptr->name, start+dz, eptr->name, start+2*dz, eptr->name, start+3*dz,
-                        eptr->name, start+2*dz, eptr->name, start+dz, eptr->name, start, eptr->name, start+3*dz);
+                fprintf(fpm, "%s %s %e 0.6666\n%s %s %e 0.6666\n%s %s %e 0\n%s %s %e -0.6666\n%s %s %e -0.6666\n%s %s %e 0\n%s %s %e 0\n",
+                        eptr->name, entity_name[eptr->type], start+dz, eptr->name, entity_name[eptr->type], start+2*dz, eptr->name, entity_name[eptr->type], start+3*dz,
+                        eptr->name, entity_name[eptr->type], start+2*dz, eptr->name, entity_name[eptr->type], start+dz, eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], start+3*dz);
                 start += 3*dz;
                 break;
             case T_MARK:    /* zero-length drift */
@@ -162,14 +169,14 @@ void output_magnets(char *filename, char *line_name, LINE_LIST *beamline)
                 end  = start+cbptr->length;
                 if (cbptr->angle>0)
                     fprintf(fpm, 
-                        "%s %e .33333333\n%s %e .33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, 
-                            eptr->name, start, eptr->name, start, eptr->name, end);
+                        "%s %s %e .33333333\n%s %s %e .33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 else if (cbptr->angle<0) 
                     fprintf(fpm, 
-                        "%s %e -.33333333\n%s %e -.33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, 
-                            eptr->name, start, eptr->name, start, eptr->name, end);
+                        "%s %s %e -.33333333\n%s %s %e -.33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_CSRCSBEND:
@@ -177,14 +184,14 @@ void output_magnets(char *filename, char *line_name, LINE_LIST *beamline)
                 end  = start+csrbptr->length;
                 if (csrbptr->angle>0)
                     fprintf(fpm, 
-                        "%s %e .33333333\n%s %e .33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, 
-                            eptr->name, start, eptr->name, start, eptr->name, end);
+                        "%s %s %e .33333333\n%s %s %e .33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 else if (csrbptr->angle<0) 
                     fprintf(fpm, 
-                        "%s %e -.33333333\n%s %e -.33333333\n%s %e  0\n%s %e  0\n%s %e  0\n%s %e 0\n",
-                            eptr->name, start, eptr->name, end, eptr->name, end, 
-                            eptr->name, start, eptr->name, start, eptr->name, end);
+                        "%s %s %e -.33333333\n%s %s %e -.33333333\n%s %s %e  0\n%s %s %e  0\n%s %s %e  0\n%s %s %e 0\n",
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end, eptr->name, entity_name[eptr->type], end, 
+                            eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], start, eptr->name, entity_name[eptr->type], end);
                 start = end;
                 break;
             case T_RFCA: case T_TWLA: case T_RAMPRF: case T_RFCW:
@@ -192,26 +199,26 @@ void output_magnets(char *filename, char *line_name, LINE_LIST *beamline)
                 dz = ((DRIFT*)eptr->p_elem)->length;
                 dz /= 8;
                 for (iPhase=0; iPhase<9; iPhase++) {
-                  fprintf(fpm, "%s %e %e\n",
-                          eptr->name, start+dz*iPhase,
+                  fprintf(fpm, "%s %s %e %e\n",
+                          eptr->name, entity_name[eptr->type], start+dz*iPhase,
                           0.5*sin((iPhase/8.0)*PIx2));
                 }
                 start += dz*8;
                 break;
             case T_MATR:
                 dz = ((MATR*)eptr->p_elem)->length;
-                fprintf(fpm, "%s %e %e\n", eptr->name, start, 0.5);
-                fprintf(fpm, "%s %e %e\n", eptr->name, start+dz, -0.5);
-                fprintf(fpm, "%s %e %e\n", eptr->name, start+dz, 0.5);
-                fprintf(fpm, "%s %e %e\n", eptr->name, start, -0.5);
-                fprintf(fpm, "%s %e %e\n", eptr->name, start, 0.0);
-                fprintf(fpm, "%s %e %e\n", eptr->name, start+dz, 0.0);
+                fprintf(fpm, "%s %s %e %e\n", eptr->name, entity_name[eptr->type], start, 0.5);
+                fprintf(fpm, "%s %s %e %e\n", eptr->name, entity_name[eptr->type], start+dz, -0.5);
+                fprintf(fpm, "%s %s %e %e\n", eptr->name, entity_name[eptr->type], start+dz, 0.5);
+                fprintf(fpm, "%s %s %e %e\n", eptr->name, entity_name[eptr->type], start, -0.5);
+                fprintf(fpm, "%s %s %e %e\n", eptr->name, entity_name[eptr->type], start, 0.0);
+                fprintf(fpm, "%s %s %e %e\n", eptr->name, entity_name[eptr->type], start+dz, 0.0);
                 start += dz;
                 break;
             default:
                 if (entity_description[eptr->type].flags&HAS_LENGTH) {
                     dz = ((DRIFT*)eptr->p_elem)->length;
-                    fprintf(fpm, "%s %e 0\n", eptr->name, start+=dz);
+                    fprintf(fpm, "%s %s %e 0\n", eptr->name, entity_name[eptr->type], start+=dz);
                     }
                 break;
             }
