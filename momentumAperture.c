a005ac25a6211fab5284e91d1c9d8764f59e2aa4
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2010-01-27 02:23:37 +0000

    Fixed warning message for number of processors vs number of elements.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@16162 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- momentumAperture.c
+++ momentumAperture.c
@@ -220,8 +220,8 @@ long doMomentumApertureSearch(
   if (nElem==0) 
     SDDS_Bomb("no elements found between s_start and s_end for momentum aperture computation");
 #else
-  if ((((output_mode?1:2)*nElem)<n_processors) && (myid==0)) {
-    printf("Warning: The number of elements should be larger than the number of processors to avoid wasting resource.\nThe number of elements is %ld. The number of processors is %d.\n", (output_mode?1:2)*nElem, n_processors);
+  if ((((output_mode==0?1:2)*nElem)<n_processors) && (myid==0)) {
+    printf("Warning: The number of elements should be larger than the number of processors to avoid wasting resource.\nThe number of elements is %ld. The number of processors is %d.\n", (output_mode==0?1:2)*nElem, n_processors);
     if (!output_mode) 
       printf("Trip: You can utilize more processors efficiently but setting output_mode=1\n");
   }    
