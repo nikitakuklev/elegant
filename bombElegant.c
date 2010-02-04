b669d35835f87d49f408e809da0535e068d9111b
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2010-02-04 16:26:59 +0000

    First version.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@16240 c1b61890-85f3-ea29-e4d6-a272fe79f168

---
+++ bombElegant.c
@@ -0,0 +1,15 @@
+/* This is not used by elegant.  It is used by other programs that use some of elegant's
+ * subroutines.  Elegant.c has a copy that is used by elegant itself.
+ */
+
+#include <stdio.h>
+
+void bombElegant(char *error, char *usage)
+{
+  if (error)
+    fprintf(stderr, "error: %s\n", error);
+  if (usage)
+    fprintf(stderr, "usage: %s\n", usage);
+  exit(1);
+}
+
