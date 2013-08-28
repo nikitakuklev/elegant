732da782227bb4fe8811ef583a807d56dd97736f
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2013-08-28 18:35:19 +0000

    First version in SVN.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@19324 c1b61890-85f3-ea29-e4d6-a272fe79f168

---
+++ mogaRing/version1/mogaConfig.tcl
@@ -0,0 +1,36 @@
+set turns 400
+set xchrom 1
+set ychrom 1
+set queue all.q
+set cores 8
+set inputList [list matchTemplate.ele optim1Template1.ele optim1Template2.ele optim1Template3.ele optim1Template4.ele]
+set modeList [list p s p p p]
+
+# Ring parameters
+set sectors 40
+
+# Error levels
+set fseError 2e-4
+set tiltError 10e-4
+set dxError 10e-6
+set dyError 10e-6
+
+# DA and MMAP parameters
+set rfHarmonic 1296
+set xmax 0.015
+set ymax 0.0015
+set s_end [expr 27.6*6]
+set name_pattern S*:S?
+
+# Script for processing jobs, plus related parameters
+set processScript processJob1
+# Largest momentum deviation (artificially imposed)
+set deltaLimit 6e-2
+# Beam parameters
+set current 100.0
+set bunches 24
+set coupling 0.01
+set sigmaz 10
+set energy 7e3
+# Set to large value (e.g., 1.0) to disable
+set clipPositive 0.007
