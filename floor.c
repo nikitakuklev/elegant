cf583e89a7af56d571a31035708858647be4280d
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2013-10-10 02:00:34 +0000

    Another fix for backward compatibility.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@19427 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- floor.c
+++ floor.c
@@ -246,7 +246,7 @@ long advanceFloorCoordinates(MATRIX *V1, MATRIX *W1, MATRIX *V0, MATRIX *W0,
   double theta0, phi0, psi0, tilt;
   static VEC *x1, *x2, *x3, *x4;
   static short x1x2Ready = 0;
-  static double sVertex = 0;
+  static double sVertex = 0, anglesVertex[3];
 #ifdef INCLUDE_WIJ
   long iw, jw;
 #endif
@@ -364,6 +364,9 @@ long advanceFloorCoordinates(MATRIX *V1, MATRIX *W1, MATRIX *V0, MATRIX *W0,
         x1->ve[1] = V0->a[1][0] - W0->a[1][2];
         x1->ve[2] = V0->a[2][0] - W0->a[2][2];
         sVertex = elem->pred->end_pos - 1;
+        anglesVertex[0] = *theta;
+        anglesVertex[1] = *phi;
+        anglesVertex[2] = *psi;
         x1x2Ready = 1;
       }
     } else {
@@ -411,7 +414,7 @@ long advanceFloorCoordinates(MATRIX *V1, MATRIX *W1, MATRIX *V0, MATRIX *W0,
           if (!SDDS_SetRowValues(SDDS_floor, SDDS_SET_BY_INDEX|SDDS_PASS_BY_VALUE, row_index,
                                  IC_S, sVertex+ds,
                                  IC_X, c->ve[0], IC_Y, c->ve[1], IC_Z, c->ve[2],
-                                 IC_THETA, *theta, IC_PHI, *phi, IC_PSI, *psi,
+                                 IC_THETA, anglesVertex[0], IC_PHI, anglesVertex[1], IC_PSI, anglesVertex[2],
                                  IC_ELEMENT, label, IC_OCCURENCE, elem->pred->occurence, IC_TYPE, "VERTEX-POINT", -1)) {
             SDDS_SetError("Unable to set SDDS row (output_floor_coordinates.1)");
             SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
