8780f2a83ba17225cf685a90ae3254c4b5419b94
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2015-03-12 22:07:01 +0000

    Added bombElegantVA---variable argument version of bombElegant.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@20867 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- elegant.c
+++ elegant.c
@@ -2592,6 +2592,82 @@ void bombTracking(char *error)
   exit(1);
 }
 
+/* This version accepts a printf-style template and variable number of arguments to be printed */
+void bombElegantVA(char *template, ...) 
+{
+  char *p;
+  char c, *s;
+  int i;
+  long j;
+  va_list argp;
+  char buffer[256];
+  float f;
+  double d;
+  
+  va_start(argp, template);
+  p = template;
+  while (*p) {
+    if (*p=='%') {
+      switch (*++p) {
+      case 'l':
+        switch (*++p) {
+        case 'd':
+          j = va_arg(argp, long int);
+          printf("%ld", j);
+          break;
+        case 'e':
+          d =  va_arg(argp, double);
+          printf("%21.15le", f);
+          break;
+        case 'f':
+          d =  va_arg(argp, double);
+          printf("%lf", f);
+          break;
+        case 'g':
+          d =  va_arg(argp, double);
+          printf("%21.15lg", f);
+          break;
+        default:
+          printf("%%l%c", *p);
+          break;
+        }
+        break;
+      case 'c':
+        c = va_arg(argp, int);
+        putchar(c);
+        break;
+      case 'd':
+        i = va_arg(argp, int);
+        printf("%d", i);
+        break;
+      case 's':
+        s = va_arg(argp, char *);
+        fputs(s, stdout);
+        break;
+      case 'e':
+        d = va_arg(argp, double);
+        printf("%21.15e", d);
+        break;
+      case 'f':
+        d = va_arg(argp, double);
+        printf("%f", d);
+        break;
+      case 'g':
+        d = va_arg(argp, double);
+        printf("%21.15g", d);
+        break;
+      default:
+        printf("%%%c", *p);
+        break;
+      }
+    }
+    else {
+      putchar(*p);
+    }
+    p++;
+  }
+  bombElegant(NULL, NULL);
+}
 
 void bombElegant(char *error, char *usage)
 {
