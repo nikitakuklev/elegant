f36873a33e29a9c0eb901e86aedec7583ef8a6e1
soliday <soliday@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2015-06-17 20:08:27 +0000

    Fixed a problem with a data type.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@21167 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- mad_parse.c
+++ mad_parse.c
@@ -1117,16 +1117,16 @@ void resetElementToDefaults(void *p_elem, long type)
   for (i=0; i<n_params; i++)  {
     switch (parameter[i].type) {
     case IS_DOUBLE:
-      *(double*)(p_elem+parameter[i].offset) = parameter[i].number;
+      *((double*)p_elem+parameter[i].offset) = parameter[i].number;
       break;
     case IS_LONG:
-      *(long*)(p_elem+parameter[i].offset) = parameter[i].integer;
+      *((long*)p_elem+parameter[i].offset) = parameter[i].integer;
       break;
     case IS_STRING:
       if (parameter[i].string==NULL)
-        *(char**)(p_elem+parameter[i].offset) = NULL;
+        *((char**)p_elem+parameter[i].offset) = NULL;
       else
-        cp_str((char**)(p_elem+parameter[i].offset), 
+        cp_str(((char**)p_elem+parameter[i].offset), 
                parameter[i].string);
       break;
     }
