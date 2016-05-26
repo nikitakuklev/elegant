0621ce75b410c726caaa521efbab1630113637d4
soliday <soliday@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2016-05-26 15:54:34 +0000

    Increased the decimal precision the output from compareGpuCpu
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@23069 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- gpuElegant/gpu_verify.cu
+++ gpuElegant/gpu_verify.cu
@@ -437,10 +437,10 @@ void compareGpuCpu(unsigned int n_part, char* name){
               << " for particle " << maxip << " component " << maxic
               << "\n  host max:";
     for(unsigned int ic=0; ic<n_comp; ic++)
-      fcout << " " << h_particles[maxip][ic];
+      fcout << std::fixed << std::setprecision(20) << " " << h_particles[maxip][ic];
     fcout << "\n  dev. max:";
     for(unsigned int ic=0; ic<n_comp; ic++)
-      fcout << " " << doh_particles[maxip][ic];
+      fcout << std::fixed << std::setprecision(20) << " " << doh_particles[maxip][ic];
     fcout << std::endl;
   }
   if (cpuNaN && gpuNaN) {
