cc5a9aeae07bc234ee6755a41f1fd405b8c5888f
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2017-02-28 15:28:03 +0000

    Getting ready to release 33.0.0.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@23877 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- track_data.c
+++ track_data.c
@@ -2866,7 +2866,7 @@ ELEMENT_DESCRIPTION entity_description[N_TYPES] = {
     { N_BRAT_PARAMS,     HAS_LENGTH,   sizeof(BRAT),  brat_param      },
     { N_BGGEXP_PARAMS,   HAS_LENGTH,   sizeof(BGGEXP),  bggexp_param      },
     { N_BRANCH_PARAMS,   0, sizeof(BRANCH),  branch_param      },
-    { N_IONEFFECTS_PARAMS, 0,     sizeof(IONEFFECTS),  ionEffects_param    },
+    { N_IONEFFECTS_PARAMS, NO_DICT_OUTPUT,     sizeof(IONEFFECTS),  ionEffects_param    },
 } ;
 
 void compute_offsets()
