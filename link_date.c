5164c78c2dd3875ba429f4dc8fece3ba858dc289
soliday <soliday@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2017-05-15 16:00:15 +0000

    It now prints the MPI runtime version.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@24096 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- link_date.c
+++ link_date.c
@@ -8,15 +8,27 @@
 \*************************************************************************/
 
 #include <stdio.h>
+#if USE_MPI 
+#include "mpi.h"
+#endif
 
 void link_date(void)
 {
+#if USE_MPI
+  int len;
+  char mpi_lib_ver[MPI_MAX_LIBRARY_VERSION_STRING];
+#endif
 #ifdef SVN_VERSION
     printf("Link date: %s %s, SVN revision: %s\n", __DATE__, __TIME__, SVN_VERSION);
 #else
     printf("Link date: %s %s\n", __DATE__, __TIME__);
 #endif
-    fflush(stdout);
+
+#if USE_MPI
+    MPI_Get_library_version(mpi_lib_ver, &len);
+    printf("MPI library version: %s\n", mpi_lib_ver);
+#endif
+fflush(stdout);
     }
 
     
