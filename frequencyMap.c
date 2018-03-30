76e72e19d7680db8149554464b441ca1d07bbf60
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2018-03-30 20:57:48 +0000

    Improved computation of diffusionRate for case when diffusion can't be determined (value is now 0).
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@25136 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- frequencyMap.c
+++ frequencyMap.c
@@ -304,7 +304,7 @@ long doFrequencyMap(
 				     IC_DIFFUSION, 
                                      diffusion,
 				     IC_DIFFUSION_RATE, 
-                                     diffusion/2-log10(turns),
+                                     diffusion==0?0:diffusion/2-log10(turns),
 				     -1)) {
 		SDDS_SetError("Problem setting SDDS row values (doFrequencyMap)");
 		SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors|SDDS_EXIT_PrintErrors);
