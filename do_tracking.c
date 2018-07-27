a85ef87a1c1340d4c08fa96faf38fa40c17650f4
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2018-07-27 13:50:05 +0000

    Fixed problem with computation of closed orbit when SCMULT elements are present.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@25436 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- do_tracking.c
+++ do_tracking.c
@@ -1661,7 +1661,7 @@ long do_tracking(
 	      track_through_lscdrift(coord, nToTrack, (LSCDRIFT*)eptr->p_elem, *P_central, charge);
 	      break;
 	    case T_SCMULT:
-	      if (getSCMULTSpecCount()) trackThroughSCMULT(coord, nToTrack, eptr);
+	      if (getSCMULTSpecCount() && !(flags&TEST_PARTICLES)) trackThroughSCMULT(coord, nToTrack, eptr);
 	      break;
 	    case T_EDRIFT:
 	      exactDrift(coord, nToTrack, ((EDRIFT*)eptr->p_elem)->length);
