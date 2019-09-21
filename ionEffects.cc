340c25c4e68a1d109843d387b26c7687678f1f38
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2019-09-21 20:41:20 +0000

    Check for invalid parameters in fitting functions.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@26493 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- ionEffects.cc
+++ ionEffects.cc
@@ -1639,9 +1639,16 @@ double multiGaussianFunction(double *param, long *invalid)
 
   *invalid = 0;
 
+  for (int i=0; i<9; i++)
+    if (isnan(param[i]) || isinf(param[i])) {
+      *invalid = 1;
+      return DBL_MAX;
+    }
+
   //param[3*j+0] = sig[j]
   //param[3*j+1] = cen[j]
   //param[3*j+2] = h[j]
+
   dx = xData[1] - xData[0];
 
   for (int i=0; i<nData; i++) {
@@ -1726,6 +1733,12 @@ double multiLorentzianFunction(double *param, long *invalid)
   double charge, dx;
 
   *invalid = 0;
+
+  for (int i=0; i<9; i++)
+    if (isnan(param[i]) || isinf(param[i])) {
+      *invalid = 1;
+      return DBL_MAX;
+    }
 
   dx = xData[1] - xData[0];
   
