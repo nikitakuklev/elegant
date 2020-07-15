6d1b98968e482d575530a5988fe703a86be37ece
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2020-07-15 22:26:46 +0000

    Moved from load_parameters.c
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@27231 c1b61890-85f3-ea29-e4d6-a272fe79f168

---
+++ patterns.c
@@ -0,0 +1,31 @@
+#include "track.h"
+
+char **addPatterns(long *patterns, char *input0) 
+{
+  char *input, *ptr, **pattern;
+  *patterns = 0;
+  if (input0==NULL)
+    return NULL;
+  cp_str(&input, input0);
+  pattern = NULL;
+  while ((ptr=get_token(input))) {
+    pattern = SDDS_Realloc(pattern, sizeof(*pattern)*(*patterns+1));
+    cp_str(&(pattern[*patterns]), ptr);
+    if (has_wildcards(pattern[*patterns]) && strchr(pattern[*patterns], '-'))
+      pattern[*patterns] = expand_ranges(pattern[*patterns]);
+    *patterns += 1;
+  }
+  free(input);
+  return pattern;
+}
+
+int matchesPatternList(char **pattern, long patterns, char *input) 
+{
+  long i;
+  for (i=0; i<patterns; i++) {
+    if (wild_match(input, pattern[i])) {
+      return 1;
+    }
+  }
+  return 0;
+}
