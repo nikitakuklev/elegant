951e6afdbd8aba946016766353aedb68c1c59d98
borland <borland@c1b61890-85f3-ea29-e4d6-a272fe79f168>
2021-07-15 16:52:09 +0000

    Fixed some units and descriptions.
    git-svn-id: https://svn.aps.anl.gov/AOP/oag/trunk/apps/src/elegant@27995 c1b61890-85f3-ea29-e4d6-a272fe79f168

--- abrat.c
+++ abrat.c
@@ -958,19 +958,19 @@ int setup_integration_output(SDDS_TABLE *SDDS_output, char *filename, char *inpu
                            SDDS_DOUBLE, (sprintf(s, "%.15e", theta), s)) < 0 ||
       SDDS_DefineParameter(SDDS_output, "rigidity", "H", "T*m", "Rigidity of electron beam", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", rigidity), s)) < 0 ||
-      SDDS_DefineParameter(SDDS_output, "FSE", "$gr$r", "", "Optimal FSE", NULL,
+      SDDS_DefineParameter(SDDS_output, "FSE", NULL, NULL, "Optimal FSE", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", fse), s)) < 0 ||
       SDDS_DefineParameter(SDDS_output, "ZVertex", "Z$bv$n", "m", "Vertex point Z coordinate", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", zVertex), s)) < 0 ||
       SDDS_DefineParameter(SDDS_output, "XVertex", "X$bv$n", "m", "Vertex point Z coordinate", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", xVertex), s)) < 0 ||
-      SDDS_DefineParameter(SDDS_output, "ZEntry", "Z$bv$n", "m", "Entry point Z coordinate", NULL,
+      SDDS_DefineParameter(SDDS_output, "ZEntry", "Z$bentry$n", "m", "Entry point Z coordinate", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", zNomEntry), s)) < 0 ||
-      SDDS_DefineParameter(SDDS_output, "XEntry", "X$bv$n", "m", "Entry point Z coordinate", NULL,
+      SDDS_DefineParameter(SDDS_output, "XEntry", "X$bentry$n", "m", "Entry point Z coordinate", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", xNomEntry), s)) < 0 ||
-      SDDS_DefineParameter(SDDS_output, "ZExit", "Z$bv$n", "m", "Exit point Z coordinate", NULL,
+      SDDS_DefineParameter(SDDS_output, "ZExit", "Z$bexit$n", "m", "Exit point Z coordinate", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", zNomExit), s)) < 0 ||
-      SDDS_DefineParameter(SDDS_output, "XExit", "X$bv$n", "m", "Exit point Z coordinate", NULL,
+      SDDS_DefineParameter(SDDS_output, "XExit", "X$bexit$n", "m", "Exit point Z coordinate", NULL,
                            SDDS_DOUBLE, (sprintf(s, "%.15e", xNomExit), s)) < 0 ||
       SDDS_DefineParameter(SDDS_output, "xf", "x$bf$n", "m", "Final x", NULL, SDDS_DOUBLE, NULL) < 0 ||
       SDDS_DefineParameter(SDDS_output, "xpf", "x'$bf$n", NULL, "Final x'", NULL, SDDS_DOUBLE, NULL) < 0 ||
